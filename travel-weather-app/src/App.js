c36c0f8a5e357f1bc94a429882e97c2ef781421e
Kenneth Jules <jules.kenneth@hotmail.com>
2023-08-15 21:24:57 -0400

    Added node_modules to .gitignore

--- travel-weather-app/src/App.js
+++ travel-weather-app/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This is created by Kenneth Jules.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
